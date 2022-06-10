010dd6f607eeac1c64f34c2170abf8c82c3444cd
syamozipc <syamozipc@gmail.com>
2022-06-10 16:51:49 +0900

    update

---
+++ archive/11_receiver.go
@@ -0,0 +1,23 @@
+package main
+
+import "log"
+
+type myStruct struct {
+	FirstName string
+}
+
+func (m *myStruct) printFirstName() string {
+	return m.FirstName
+}
+
+func main(){
+	var myVar myStruct
+	myVar.FirstName = "ryota"
+
+	myVar2 := myStruct {
+		FirstName: "saaya",
+	}
+
+	log.Println("myVar is set to", myVar.printFirstName())// myVar is set to ryota
+	log.Println("myVar2 is set to", myVar2.printFirstName())// myVar2 is set to saaya
+}
