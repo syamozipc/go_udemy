59cb874483cd5ae1f4f139fd84615751730b8b51
syamozipc <syamozipc@gmail.com>
2022-06-13 15:31:46 +0900

    update

--- archive/16_go.mod
+++ archive/16_go.mod
@@ -1,3 +1,4 @@
+// 必要に応じてgo.modに戻す
 module github.com/syamozipc/my_nice_program
 
 go 1.18
--- main.go
+++ main.go
@@ -5,8 +5,19 @@ import (
 	"net/http"
 )
 
+//
 func main() {
+	// http requestを受け取った際に実行される処理
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		fmt.Fprintf(w,"hello world")
+		n, err := fmt.Fprintf(w,"hello world!")
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
 	})
+
+	// serverを http://localhost:8080 に立ち上げる
+	// 引数を_にすることで、errorがreturnされた場合、それをスルーする
+	_ = http.ListenAndServe(":8080", nil)
 }
